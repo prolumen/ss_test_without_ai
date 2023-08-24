34d25b0bb2d4568ae25a947c469d77019d1508bc
Andrii Prytula <a.s.pritula@gmail.com>
2023-08-24 15:18:02 +0300

    C#1

---
+++ src/main/java/com/example/ss_test_without_ai/SsTestWithoutAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ss_test_without_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsTestWithoutAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsTestWithoutAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ss_test_without_ai/SsTestWithoutAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss_test_without_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsTestWithoutAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
